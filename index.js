a23b9660f86ae3a542d0a20d6be5ffa5af9738a5
Alice Frumin <alifrumin@yahoo.com>
2015-12-01 10:33:05 -0500

    updated index.js to include edit and update routes

--- index.js
+++ index.js
@@ -24,14 +24,12 @@ app.get("/compliments/new", complimentsController.new);
 
 // show
 app.get("/compliments/:id", complimentsController.show);
-
-
 // edit
   // edit route code goes here...
-
+app.get("/compliments/:id/edit", complimentsController.edit);
 // update
  // update route code goes here...
-
+app.put("/compliments/:id", authorsController.update)
 // Run our app on Port 3000
 app.listen(3000, function(){
   console.log("Listening on http://localhost:3000/");
